a98bd5382e3cbd168a3184467d8cd301f1ecfcde
Eric Swanson <54718953+eswanson97@users.noreply.github.com>
2023-11-15 12:04:43 -0500

    added start game functionality

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     public void StartGame()
     {
